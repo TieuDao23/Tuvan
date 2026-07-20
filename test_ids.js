c376f34d32f02efed56e4a9b1c788e0ee93b4b0a
Tieu Dao <duyanhblt1@gmail.com>
2026-07-20 11:51:28 +0700

    Dọn dẹp các tệp tin không sử dụng

--- test_ids.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const app = fs.readFileSync('app.js', 'utf8');
-const m = app.matchAll(/\$\(\'#([a-zA-Z0-9_-]+)\'\)/g);
-const ids = new Set(Array.from(m, x => x[1]));
-for (let id of ids) {
-    if (!html.includes('id="' + id + '"')) console.log('Missing: ' + id);
-}
